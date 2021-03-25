e4af6be893bd899b7b87e7e70982d46f5b18f3dc
Aus Gomez <austrebertog@gmail.com>
2021-03-25 17:36:25 -0500

    init

---
+++ src/tictactoe.cpp
@@ -0,0 +1,15 @@
+/* My first contract!! yay! */
+/* Aus Gomez */
+
+#include <eosio/eosio.hpp>
+using namespace eosio;
+
+CONTRACT tictactoe : public contract {
+  public:
+    using contract::contract;
+
+    ACTION welcome( name host, name challenger ) {
+      require_auth( get_self() );
+      print("Welcome, challengers ", host, " and ", challenger, "!");
+    }
+};
